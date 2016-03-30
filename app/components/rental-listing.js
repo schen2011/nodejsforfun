833a9f2267346046673a41bca1aa1f5a8a1bfc87
stephen-chen-velir <stephen.chen@velir.com>
2016-03-30 14:55:35 -0400

    ember js for fun

---
+++ app/components/rental-listing.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  	isImageShowing: false,
+    actions: {
+	    imageShow() {
+	      this.set('isImageShowing', true);
+    	}
+    }
+});
